2029193d8749ce5fd6b6aefbc69794133a1121f3
r8bbers <220712030@students.uajy.ac.id>
2024-10-03 15:28:04 +0700

    List Cinema

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:tubes_pbp_9/view/list_view.dart';
+=======
+import 'package:tubes_pbp_9/view/view_list.dart';
+>>>>>>> Stashed changes
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -10,18 +14,33 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   int _selectedIndex = 0;
+<<<<<<< Updated upstream
   late List<Widget> _widgetOptions;
+=======
+
+>>>>>>> Stashed changes
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
+<<<<<<< Updated upstream
   @override
   Widget build(BuildContext context) {
     _widgetOptions = <Widget>[
       ListPageView(),
     ];
+=======
+  static const List<Widget> _widgetOptions = <Widget>[
+    ListPageView(),
+    ListPageView(),
+    ListPageView(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+>>>>>>> Stashed changes
     return Scaffold(
       appBar: AppBar(
         title: const Text('Atmacinema'),
@@ -49,7 +68,11 @@ class _HomeViewState extends State<HomeView> {
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
       ),
+<<<<<<< Updated upstream
       body: _widgetOptions.elementAt(_selectedIndex),
+=======
+      body: _widgetOptions[_selectedIndex],
+>>>>>>> Stashed changes
     );
   }
 }
