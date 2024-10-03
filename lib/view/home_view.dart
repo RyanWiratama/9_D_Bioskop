d7bec61bb3039e8e61b71f2d25f34b7e67dfaaad
RyanWiratama <220711897@students.uajy.ac.id>
2024-10-03 15:24:37 +0700

    home page

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -9,27 +9,63 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
 
+<<<<<<< Updated upstream
+=======
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Center(
+      child: Image(image: NetworkImage(''))
+    ),
+    // PageListView();
+    Center(
+      child: Text(
+        'Index 3: Profile',
+      )
+    )
+  ];
+
+>>>>>>> Stashed changes
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Atmacinema'),
         centerTitle: true,
+<<<<<<< Updated upstream
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.person),
             onPressed: () {},
           ),
         ],
+=======
+>>>>>>> Stashed changes
       ),
       backgroundColor: const Color.fromARGB(255, 19, 36, 66),
       bottomNavigationBar: BottomNavigationBar(
       items: const[
          BottomNavigationBarItem(icon: Icon(Icons.home,),label: 'Home'),
+<<<<<<< Updated upstream
          BottomNavigationBarItem(icon: Icon(Icons.movie,),label: 'Ticket'),
          BottomNavigationBarItem(icon: Icon(Icons.restaurant,),label: 'FnB'),
       ],
     ),
+=======
+         BottomNavigationBarItem(icon: Icon(Icons.movie,),label: 'List'),
+         BottomNavigationBarItem(icon: Icon(Icons.person,),label: 'Profile'),
+      ],
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+    ),
+    body: _widgetOptions.elementAt(_selectedIndex),
+>>>>>>> Stashed changes
     );
   }
 }
---
+++ lib/view/register_view.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+import 'package:tubes_pbp_9/view/login_view.dart';
+import 'package:tubes_pbp_9/component/form_component.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController notelpController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+      appBar: AppBar(
+        title: const Text('Register'),
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const LoginView()),
+            );
+          },
+        ),
+      ),
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Username',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                inputForm(
+                  (p0) {
+                    if (p0 == null || p0.isEmpty) {
+                      return 'Required Username!';
+                    }
+                    return null;
+                  },
+                  controller: usernameController,
+                  hintTxt: "Username",
+                  helperTxt: "Enter Username",
+                  iconData: Icons.person,
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  'Email',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                inputForm(
+                  (p0) {
+                    if (p0 == null || p0.isEmpty) {
+                      return 'Required Email!';
+                    }
+                    if (!p0.contains('@')) {
+                      return 'Email Must Contain @';
+                    }
+                    return null;
+                  },
+                  controller: emailController,
+                  hintTxt: "Email",
+                  helperTxt: "Enter Email Adresses",
+                  iconData: Icons.email,
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  'Password',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                inputForm(
+                  (p0) {
+                    if (p0 == null || p0.isEmpty) {
+                      return 'Required Password!';
+                    }
+                    if (p0.length < 8) {
+                      return 'Required 8 digits Password!';
+                    }
+                    if (!RegExp(r'[A-Z]').hasMatch(p0)) {
+                      return 'Password must contain at least one uppercase letter!';
+                    }
+                    // Cek jika tidak mengandung angka
+                    if (!RegExp(r'[0-9]').hasMatch(p0)) {
+                      return 'Password must contain at least one number!';
+                    }
+                    return null;
+                  },
+                  controller: passwordController,
+                  hintTxt: "Password",
+                  helperTxt: "Enter Password",
+                  iconData: Icons.password,
+                  password: true,
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  'Phone Number',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                inputForm(
+                  (p0) {
+                    if (p0 == null || p0.isEmpty) {
+                      return 'Required Phone Number!';
+                    }
+                    return null;
+                  },
+                  controller: notelpController,
+                  hintTxt: "Phone Number",
+                  helperTxt: "Enter Phone Number",
+                  iconData: Icons.phone_android,
+                ),
+                const SizedBox(height: 32),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        Map<String, dynamic> formData = {};
+                        formData['username'] = usernameController.text;
+                        formData['password'] = passwordController.text;
+
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => LoginView(
+                              data: formData,
+                            ),
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.7,
+                        color: Colors.grey.withOpacity(0.5),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 0,
+                        horizontal: 10,
+                      ),
+                      child: Text(
+                        'Sign up with',
+                        style: TextStyle(
+                          color: Colors.black45,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.7,
+                        color: Colors.grey.withOpacity(0.5),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.g_mobiledata, size: 40),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.facebook, size: 40),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
