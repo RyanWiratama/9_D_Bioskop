36d4eb082e1ceb1edae449ba3e6e6698925997df
dioftsunami <220711694@students.uajy.ac.id>
2024-12-03 17:15:21 +0700

    Membuat Consume API (Movie, Movie Detail)

--- lib/requests/filmReq.dart
+++ lib/requests/filmReq.dart
@@ -1,20 +1,28 @@
 import 'package:tubes_pbp_9/entity/film.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class FilmReq {
   static final String baseUrl = '10.0.2.2:8000';
   static final String endpoint = '/api/film';
+  final FlutterSecureStorage storage = FlutterSecureStorage();
 
-  static Future<List<Film>> fetchByGenre(String genre) async {
-    final Uri url = Uri.http(baseUrl, endpoint, {'genre': genre});
+  static Future<List<Film>> fetchAllFilms() async {
+    final Uri url = Uri.http(baseUrl, endpoint);
 
     try {
+      final String? token = await FlutterSecureStorage().read(key: 'token');
+
+      if (token == null) {
+        throw Exception('User is not authenticated');
+      }
+
       final response = await http.get(
         url,
         headers: {
-          'Authorization':
-              'Bearer 1|M3X3sEQpmXxLLAHD9POWYCdV6u7ds06C5laNIWq751027286',
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
         },
       );
 
@@ -23,6 +31,34 @@ class FilmReq {
         return jsonData.map((data) => Film.fromJson(data)).toList();
       } else {
         throw Exception('Failed to load data: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+
+  static Future<Film> fetchFilmById(int id) async {
+    final Uri url = Uri.http(baseUrl, '$endpoint/$id');
+
+    try {
+      final String? token = await FlutterSecureStorage().read(key: 'token');
+
+      if (token == null) {
+        throw Exception('User is not authenticated');
+      }
+
+      final response = await http.get(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return Film.fromJson(jsonDecode(response.body));
+      } else {
+        throw Exception('Failed to load film: ${response.reasonPhrase}');
       }
     } catch (e) {
       throw Exception('Error: $e');
